f614b4580ab52372b7ea82df1f4a44ca2d733934
teju <ksweetteju00@gmail.com>
2024-02-08 22:04:46 +0530

    TRP-4563 : Adding comment for driver

--- src/main/java/selenium/basics/FirstAutomationTestcase.java
+++ src/main/java/selenium/basics/FirstAutomationTestcase.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstAutomationTestcase {
 
+	//defining driver
         static WebDriver driver = null;
 
         public static void main(String[] args) {
