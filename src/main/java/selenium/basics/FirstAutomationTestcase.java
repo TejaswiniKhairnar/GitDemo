47543a9a914daee2af165c0f94fc04ac04c5c3d0
teju <ksweetteju00@gmail.com>
2024-02-08 22:08:13 +0530

    TRP-9098

--- src/main/java/selenium/basics/FirstAutomationTestcase.java
+++ src/main/java/selenium/basics/FirstAutomationTestcase.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstAutomationTestcase {
 
-	//defining driver
+	//defining driver 123
         static WebDriver driver = null;
 
         public static void main(String[] args) {
