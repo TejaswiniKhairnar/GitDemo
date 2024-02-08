6a76b73512ce1b157ba4f715f6dd0d55d76ee227
tejuK1993 <159434929+tejuK1993@users.noreply.github.com>
2024-02-08 22:06:37 +0530

    Update Locators.java

--- src/main/java/com/teju/patil/Locators.java
+++ src/main/java/com/teju/patil/Locators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
-	/*
+	/****
 	========================================================
             Selenium - Locators ( cssSelector)
 ========================================================
